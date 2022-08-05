547d66d5c76f84f0e6898331b5837d34f50f3792
sarahandromeda <SarahAndromeda@protonmail.ch>
2022-08-04 18:03:56 -0600

    Updated allowed hosts.

--- employee_register/settings.py
+++ employee_register/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://business-registry.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
